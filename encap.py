905f8d15d266f976645df380f1ca28e01d00fe61
asif <shaikasif15062000@gmail.com>
2021-11-27 19:39:59 +0530

    hello

---
+++ encap.py
@@ -0,0 +1,13 @@
+class rectangle:
+    __length=0
+    __breadth=0
+
+    def __init__(self):
+        self.__length=5
+        self.__breadth=3
+
+        print(self.__length)
+        print(self.__breadth)
+object=rectangle()
+print(i=object.length)
+print(object.breadth)
